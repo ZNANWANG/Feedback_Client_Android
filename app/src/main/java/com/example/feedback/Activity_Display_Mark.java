ee5945784e574db13e617e3889a095eb0ed482ea
ZNANWANG <znanwang321@gmail.com>
2019-10-22 16:52:55 +1100

    "fix bugs"

--- app/src/main/java/com/example/feedback/Activity_Display_Mark.java
+++ app/src/main/java/com/example/feedback/Activity_Display_Mark.java
@@ -199,7 +199,7 @@ public class Activity_Display_Mark extends AppCompatActivity {
             button_viewReport.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (from.equals(Activity_Assessment.FROMREALTIME)) {
+                    if (from.equals(Activity_Assessment.FROMREALTIME) || from.equals(Activity_Send_Report_Individual.FROMREALTIMESEND)) {
                         if (indexOfGroup == -999) {
                             Intent intent = new Intent(Activity_Display_Mark.this, Activity_Editable_Individual_Report.class);
                             intent.putExtra("indexOfProject", String.valueOf(indexOfProject));
@@ -217,7 +217,7 @@ public class Activity_Display_Mark extends AppCompatActivity {
                             intent.putExtra("from", FROMREALTIME);
                             startActivity(intent);
                         }
-                    } else if (from.equals(FROMREVIEW)) {
+                    } else if (from.equals(FROMREVIEW) || from.equals(Activity_Send_Report_Individual.FROMREVIEWSEND)) {
                         if (indexOfGroup == -999) {
                             Intent intent = new Intent(Activity_Display_Mark.this, Activity_Editable_Individual_Report.class);
                             intent.putExtra("indexOfProject", String.valueOf(indexOfProject));
